aa52440363db9b674d35167432ae47aa73880425
xexi81 <74619705+xexi81@users.noreply.github.com>
2021-01-17 07:29:47 +0100

    v3: Registro y login en Firebase funcionando

--- app/src/main/java/com/xexi/conecta4Login/data/login/RepoImplGetUserLogged.kt
+++ app/src/main/java/com/xexi/conecta4Login/data/login/RepoImplGetUserLogged.kt
@@ -1,11 +1,14 @@
 package com.xexi.conecta4Login.data.login
 
-import android.util.Log
+
 import com.google.firebase.auth.FirebaseAuth
 import com.xexi.conecta4Login.base.Resource
+import kotlinx.coroutines.tasks.await
 
 class RepoImplGetUserLogged: IRepoGetUserLogged {
     private val mAuth: FirebaseAuth = FirebaseAuth.getInstance();
+
+    // Obtain user
     override suspend fun getUserLogged(): Resource<User> {
 
         val currentUser = mAuth.currentUser
@@ -23,4 +26,46 @@ class RepoImplGetUserLogged: IRepoGetUserLogged {
         }
 
     }
+
+
+    // Register an user on firebase
+    override suspend fun registerUser(email: String, password: String): Resource<Boolean> {
+        // Firebase create user with email and pass
+        try {
+            mAuth.createUserWithEmailAndPassword(email, password).await()
+            return Resource.Success(true)
+        } catch (e: Exception) {
+            return Resource.Success(false)
+        }
+    }
+
+
+    // Login an user on firebase
+    override suspend fun loginUser(email: String, password: String): Resource<Boolean> {
+        // Firebase create user with email and pass
+        try {
+            mAuth.signInWithEmailAndPassword(email, password).await()
+            return Resource.Success(true)
+        } catch (e: Exception) {
+            return Resource.Success(false)
+        }
+    }
+
+
+    // Login an user on Google
+    override suspend fun loginGoogleUser(email: String, password: String): Resource<Boolean> {
+
+        // Firebase create user with email and pass
+        val result = mAuth.signInWithEmailAndPassword(email, password)
+
+        // Waiting until result is complete
+        while(!result.isComplete) {
+        }
+
+        return if (result.isSuccessful) {
+            Resource.Success(true)
+        } else Resource.Success(false)
+
+    }
+
 }
